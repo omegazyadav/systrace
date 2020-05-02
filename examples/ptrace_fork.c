fa33f7a3117fc79e3d230e04814b1c1e26201035
Yadav Lamichhane <omegazyadav1@gmail.com>
2020-05-02 22:41:11 +0545

    storing the syscall number into %RAX register

--- examples/ptrace_fork.c
+++ examples/ptrace_fork.c
@@ -2,13 +2,15 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/ptrace.h>
+#include <sys/reg.h>
 #include <sys/types.h>
 #include <sys/user.h>
 
 int main(int argc, char *argv[]) 
 {
     pid_t pid;
-    long ret;
+    int wstatus;
+    long orig_eax;
     struct user_regs_struct value;
 
     pid = fork();
@@ -19,9 +21,10 @@ int main(int argc, char *argv[])
         // Perform a syscall after ptrace call.     
         execl("/bin/ls", "ls", NULL);
     } else {
-        wait(NULL);
-        ret = ptrace(PTRACE_PEEKUSER, pid, 4*value.orig_rax, NULL);
-        printf("Child executed a system call with number %ld\n",ret);
+        waitpid(pid, &wstatus, 0);
+      	// wait(NULL);
+        orig_eax = ptrace(PTRACE_PEEKUSER, pid, 8 * ORIG_RAX, NULL);
+        printf("Child executed a system call with number %ld\n",orig_eax);
         ptrace(PTRACE_CONT, pid, NULL, NULL);
 
     }
